796b4705c97baa66ee3ffaac2d3e0317ce269005
Zahan Malkani <zahanm@gmail.com>
2015-09-16 18:05:50 -0700

    [server] creating and getting info on links

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/articles');
 const app = koa();
 
-import { Thread, User } from './models';
+import { Thread, Link, User } from './models';
 import { nonEmptyString } from './utils';
 
 app.use(bodyParser());
@@ -85,6 +85,37 @@ router.get('/links', function *(next) {
     return;
   }
   this.body = t.contents;
+});
+
+router.get('/link', function *(next) {
+  this.assert(this.user, 401, 'need to be authenticated');
+  this.assert(nonEmptyString(this.request.query.id), 400, 'need a link ID');
+  const l = yield Link.findById(this.request.query.id).exec();
+  if (!l) {
+    this.status = 404;
+    return;
+  }
+  this.body = l;
+});
+
+router.post('/link', function *(next) {
+  this.assert(this.user, 401, 'need to be authenticated');
+  this.assert(nonEmptyString(this.request.body.id), 400, 'need a thread ID');
+  this.assert(nonEmptyString(this.request.body.url), 400, 'need a URL');
+  const t = yield Thread.findById(this.request.body.id).exec();
+  if (!t) {
+    this.throw(400, 'need a valid thread ID');
+  }
+  let link = yield Link.findOne({ url: this.request.body.url }).exec();
+  if (!link) {
+    link = new Link({
+      url: this.request.body.url,
+    });
+    yield link.save();
+  }
+  t.contents.push(link._id);
+  yield t.save();
+  this.status = 201;
 });
 
 app.use(router.routes());
